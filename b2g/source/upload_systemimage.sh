b212e46ab14ba01dd3ddc1d7ef8abac37b14650a
ittat <ittat@live.com>
2021-02-20 18:45:40 +0800

    Update upload_systemimage.sh

--- b2g/source/upload_systemimage.sh
+++ b2g/source/upload_systemimage.sh
@@ -2,5 +2,7 @@
 set -e 
         cd  ${work}
         xz -c  B2G/out/target/product/${device_name}/system.img > ~/${systemimage}
+        xz -c  B2G/out/target/product/${device_name}/boot.img > ~/boot.img.xz
         cd ~
         rclone copy system.img.xz itd:ci/${remotepath}
+        rclone copy boot.img.xz itd:ci/${remotepath}
